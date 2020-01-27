c81e19177703d0f376165caff925faf5249a14d8
Kmilka <do.kseniya@gmail.com>
2020-01-27 20:02:59 +0200

    fetched spells and fixed styling issues, added filter on name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import cat from './halloween-black-cat.svg';
-import spell from './spell.svg';
+import spellPicture from './spell.svg';
+import skull from './skull.svg';
+import magic from './halloween.svg';
+import star from './star.svg';
+import student from './student.svg';
+import brain from './brain.svg';
 
 
 class App extends Component {
@@ -10,17 +15,32 @@ class App extends Component {
     super();
     this.state = {
       characters: [],
+      searchField: "",
       spells: [],
+      flagCastASpell: false,
+      flagSpellInAction: false,
+      randonSpellNumber: null,
     };
 
   }
 
   handleChange(event) {
-    console.log(this.state.characters);
+    this.setState({
+      searchField: event.target.value,
+      })
   }
 
   handleClick() {
+    this.setState({
+      flagCastASpell: true,
+      randomSpellNumber: Math.round(Math.random() * 150, 0),
+    })
+  }
 
+  castSpell() {
+    this.setState({
+      flagSpellInAction: true,
+    })
   }
 
   componentDidMount() {
@@ -41,9 +61,16 @@ class App extends Component {
   }
 
   render() {
-    const {characters} = this.state;
+    const {characters, spells, flagCastASpell, flagSpellInAction, randomSpellNumber} = this.state;
+    const filteredCharacters = this.state.characters.filter(value =>
+      value.name.toLocaleLowerCase().includes(this.state.searchField.toLocaleLowerCase()));
+    const spell = spells[randomSpellNumber];
+    const CastASpellClass = flagCastASpell ? "cast-spell" : "invisible";
+    let spellBtnImg = ["pick-spell"];
+    spellBtnImg.push(!flagCastASpell ? "before-click" : "after-click");
+    const animateSpell = flagSpellInAction ? "animate-spell" : "";
     return (
-      !characters.length ?
+      (!characters.length || !spells.length)?
       <h1>Loading</h1> :
       <div>
         <header>
@@ -51,19 +78,27 @@ class App extends Component {
             Search for a certain person from <span>Harry Potter</span>
             <img src={cat} alt="cat" /> universe and
             <br/>
-            <img src={spell} alt="spell" />
+            <img src={spellPicture} alt="spell" />
             &nbsp; cast a spell on him or her
           </h1>
-          <div className="search">
-            <input type="text" placeholder="  Type name" onChange={(event) => this.handleChange(event)}></input>
-            <button onClick={() => this.handleClick}></button>
-          </div>
         </header>
-        <CardList props={this.state.characters} />
+        <div className="flex-column center">
+          <input type="text" placeholder="  Type name" onChange={(event) => this.handleChange(event)}></input>
+            <div className="flex-column center">
+              <span className={animateSpell}>{flagCastASpell ? spell.spell : ""}</span>
+              <p>{flagCastASpell ? spell.effect : ""}</p>
+            <div className="flex-row center">
+              <button className={spellBtnImg.join(" ")} onClick={() => this.handleClick()}></button>             
+              <button className={CastASpellClass} onClick={() => this.castSpell()}>Cast this spell</button>
+            </div>
+        </div>
+      </div>
+
+        <CardList props= {filteredCharacters} />
+
       </div>
     )
   }
-
 }
 
 function CardList ( listOfCharacters ) {
@@ -84,27 +119,27 @@ function CardList ( listOfCharacters ) {
 function Card (props) {
   const { name, role, house, ministryOfMagic, orderOfThePhoenix, dumbledoresArmy, deathEater, bloodStatus, species } = props.character;
   const teamPlayer = [];
-  deathEater ? teamPlayer.push("DeathEater") : teamPlayer.push("");
+  deathEater && role === "" ? teamPlayer.push("DeathEater") : teamPlayer.push("");
   ministryOfMagic ? teamPlayer.push("Worked in the Ministry of Magic") : teamPlayer.push("");
   dumbledoresArmy ? teamPlayer.push("Joined the Dumbldore's Army") : teamPlayer.push("");
   orderOfThePhoenix ? teamPlayer.push("One of the Order of the Phoenix") : teamPlayer.push("");
 
   const listItems = teamPlayer.map((value, index) => {
       if (value !== "") {
-        return <li>{value}</li>        
+        return <li key={index}>{value}</li>        
       } else return null;
   });
 
   return (
     <div className="card">
-      <h1>{name}</h1>
-      <h2>{role}</h2>
-      <hr>
-      </hr>
+      <div className="card-heading">
+        <h1>{name}</h1>
+        <h2>{role ? role : (deathEater ? "DeathEater" : "Other")}{ house ? ", " + house : ""}</h2>
+      </div>
+      <img src={ role === 'student' ? student : (deathEater ? skull : (bloodStatus === 'muggle' ? brain : (species === 'human' ? magic : star)))} alt="role description" />
       <ul>
         <li>Species: {species}</li>
         <li>Bloodstatus: {bloodStatus}</li>
-        <li className={ !house ? "invisible" : ""} >Hogwarts house: {house}</li>
       </ul>
         <ul>
           {listItems}
