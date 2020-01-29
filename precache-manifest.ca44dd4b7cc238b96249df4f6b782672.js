27e6c94b329c19a2af2951ca7fada26713ae58b3
Kmilka <do.kseniya@gmail.com>
2020-01-29 18:40:51 +0200

    Updates

---
+++ precache-manifest.ca44dd4b7cc238b96249df4f6b782672.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d7afa8ce0fcb0038e40837ccd040747",
+    "url": "/ZtM-sideproject-HP/index.html"
+  },
+  {
+    "revision": "40e4b6c372bc5c3d3d4a",
+    "url": "/ZtM-sideproject-HP/static/css/main.0853415f.chunk.css"
+  },
+  {
+    "revision": "a6135f0ac414fb6582b3",
+    "url": "/ZtM-sideproject-HP/static/js/2.5f3a5b87.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ZtM-sideproject-HP/static/js/2.5f3a5b87.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40e4b6c372bc5c3d3d4a",
+    "url": "/ZtM-sideproject-HP/static/js/main.5ca5e844.chunk.js"
+  },
+  {
+    "revision": "26aadbbdf7e89f9651c3",
+    "url": "/ZtM-sideproject-HP/static/js/runtime-main.6f45a5c4.js"
+  },
+  {
+    "revision": "014560c03cbcab7f1adee1b2824cc5d6",
+    "url": "/ZtM-sideproject-HP/static/media/brain.014560c0.svg"
+  },
+  {
+    "revision": "b94540416a0aeff2a9491719e9b002d9",
+    "url": "/ZtM-sideproject-HP/static/media/halloween-black-cat.b9454041.svg"
+  },
+  {
+    "revision": "a08c5f5a0517ae587cf71b7144b8be3c",
+    "url": "/ZtM-sideproject-HP/static/media/halloween.a08c5f5a.svg"
+  },
+  {
+    "revision": "f0e41cae85f6a7527cd1edcc2ba48fb8",
+    "url": "/ZtM-sideproject-HP/static/media/refresh.f0e41cae.svg"
+  },
+  {
+    "revision": "343185ea007e169d5df3b155fcbf4d89",
+    "url": "/ZtM-sideproject-HP/static/media/skull.343185ea.svg"
+  },
+  {
+    "revision": "12d79651f009b4e813de7e25914f5e78",
+    "url": "/ZtM-sideproject-HP/static/media/spell.12d79651.svg"
+  },
+  {
+    "revision": "dfe50558cf9991076dc8d81607ccb80b",
+    "url": "/ZtM-sideproject-HP/static/media/star.dfe50558.svg"
+  },
+  {
+    "revision": "a1ff269b1de0443b329cfb747f78c7d6",
+    "url": "/ZtM-sideproject-HP/static/media/student.a1ff269b.svg"
+  },
+  {
+    "revision": "a35beab3b96b42ae0a450e34a051f5ba",
+    "url": "/ZtM-sideproject-HP/static/media/wizard.a35beab3.svg"
+  }
+]);
